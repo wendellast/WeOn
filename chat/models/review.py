812a3da9773f2a1f6840d1181f235f6191f7b960
wendellast <wendelalves01a@hotmail.com>
2025-02-14 16:05:25 -0300

    feat: category model

--- chat/models/review.py
+++ chat/models/review.py
@@ -12,8 +12,10 @@ class Review:
     id = Column(String, primary_key=True, index=True, default=lambda: shortuuid.uuid())
     text = Column(String, nullable=False)
     sentiment = Column(String, nullable=False)
+    category = Column(String, nullable=False)
     created_at = Column(DateTime, server_default=func.now(), nullable=False)
 
-    def __init__(self, text, sentiment):
+    def __init__(self, text, sentiment, category):
         self.text = text
         self.sentiment = sentiment
+        self.category = category
